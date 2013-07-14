56d0f104548982699371672520db9c107af740ca
Laurent Chedanne <laurent@chedanne.pro>
2013-07-14 03:06:39 +0200

    jquery ui Correction home si pas ouvert widget application google prettify

---
+++ Resources/public/js/application.js
@@ -0,0 +1,31 @@
+(function( $ ) {
+	$.widget( "ui.application", {
+		_create: function() {},
+		_init: function() {
+			try {
+				// Disable certain links in docs
+				this.element.find('section [href^=#]').click(function (e) {
+					if (!($(this).hasClass('internal')))
+						e.preventDefault();
+			    });
+				this.element.find('.input-required').each(function() {
+					$(this).css({'cursor': 'pointer' });
+					$(this).tooltip({ placement: 'top' });
+				});
+			    // tooltip / popover
+				this.element.find('[data-toggle="tooltip"]').tooltip();
+				this.element.find('[data-toggle="popover"]').popover();
+			} catch (e) {
+				(typeof(console) !== "undefined" && console.log) && console.log(e);
+			}
+		},
+	});
+})( jQuery );
+
+// JAVASCRIPT INIT UI
+// ++++++++++++++++++++++++++++++++++++++++++
+$(document).ready(function() {
+	// make code pretty
+    window.prettyPrint && prettyPrint();
+    $(this).application();
+});
